32e41325df11e694eacf231dd30b586d067eb488
anthonyrebolledo <4nto.rebolledo@gmail.com>
2025-04-22 17:45:45 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,65 +1,54 @@
-
 import requests
-
 from dotenv import load_dotenv
-
 import os
 
+def CargarConfiguracion():
+    load_dotenv()
+    api_key = os.getenv("API_KEY_SEARCH_GOOGLE")
+    search_engine_id = os.getenv("SEARCH_ENGINE_ID")
+    if not api_key or not search_engine_id:
+        print("Error: Las variables de entorno no están configuradas correctamente.")
+        exit()
+    return api_key, search_engine_id
+def ConstruirUrl(api_key, search_engine_id, query, page, lang):
+    return f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={search_engine_id}&q={query}&start={page}&lr={lang}"
+def RealizarBusqueda(url):
+    """Realiza la solicitud a la API y devuelve los resultados."""
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error al realizar la solicitud: {e}")
+        return None
 
+def procesar_resultados(resultados):
+    if not resultados:
+        print("No se encontraron resultados.")
+        return
 
-load_dotenv()
-
-API_KEY = os.getenv("API_KEY_SEARCH_GOOGLE")
-
-SEARCH_ENGINE_ID = os.getenv("SEARCH_ENGINE_ID")
-
-
-
-# configuracion de la consulta y los parametros de busqueda
-
-query = 'filetype:sql "MySQL dump" (pass|password|passwd|pwd)'
-
-page = 1
-
-lang = "lang_es"
-
-# Construcción de la URL para la API de Google Custom Search
-
-url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={page}&lr={lang}"
-
-# Realizar la solicitud a la API
-
-response = requests.get(url)
-
-data = response.json()
-
-
-
-result= data.get('items', [])
-
-if not result:
-
-    print("No se encontraron resultados.")
-
-else:
-
-    for item in result:
-
+    for item in resultados:
         title = item.get('title')
-
         link = item.get('link')
-
         snippet = item.get('snippet')
-
         print(f"Title: {title}")
-
         print(f"Link: {link}")
-
         print(f"Snippet: {snippet}")
-
         print("-" * 80)
 
-        
+def main():
+    api_key, search_engine_id = CargarConfiguracion()
 
+    query = 'filetype:sql "MySQL dump" (pass|password|passwd|pwd)'
+    page = 1
+    lang = "lang_es"
 
+    url = ConstruirUrl(api_key, search_engine_id, query, page, lang)
 
+    data = RealizarBusqueda(url)
+    if data:
+        resultados = data.get('items', [])
+        procesar_resultados(resultados)
+
+if __name__ == "__main__":
+    main()
